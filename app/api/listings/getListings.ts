c99f59afa4f388309a73ca6b9133a30080f13cee
Matt Kornelsen <matt@kornelsen.dev>
2024-05-12 14:07:13 -0400

    added api uri env variable

--- app/api/listings/getListings.ts
+++ app/api/listings/getListings.ts
@@ -2,17 +2,17 @@ import type { TFilters, TListing } from "@/app/_types";
 import { useQuery, keepPreviousData } from "@tanstack/react-query";
 
 export const getListings = async (filters: TFilters) => {
-	const filtersParam = encodeURIComponent(JSON.stringify(filters));
-	const uri = `http://localhost:3000/api/listings?filters=${filtersParam}`;
-	const resp = await fetch(uri, { cache: "no-store" });
-	const result = await resp.json();
-	return result;
+  const filtersParam = encodeURIComponent(JSON.stringify(filters));
+  const uri = `${process.env.NEXT_PUBLIC_API_URI}listings?filters=${filtersParam}`;
+  const resp = await fetch(uri, { cache: "no-store" });
+  const result = await resp.json();
+  return result;
 };
 
 export const useListings = (filters: TFilters) => {
-	return useQuery<TListing[]>({
-		queryKey: ["listings", encodeURIComponent(JSON.stringify(filters))],
-		queryFn: () => getListings(filters),
-		placeholderData: keepPreviousData,
-	});
+  return useQuery<TListing[]>({
+    queryKey: ["listings", encodeURIComponent(JSON.stringify(filters))],
+    queryFn: () => getListings(filters),
+    placeholderData: keepPreviousData,
+  });
 };
