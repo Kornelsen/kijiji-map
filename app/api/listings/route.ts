2bc7fc3aef64f067e5eff1d340192a5056efc7eb
kornelsen <matt@kornelsen.dev>
2024-05-13 09:43:14 -0400

    move dynamic data outside of listing get try catch statement

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -4,15 +4,18 @@ import type { TFilters, TListing } from "../../_types";
 import { getFilters } from "./utils";
 
 export async function GET(req: Request) {
+	const url = new URL(req.url);
+	const searchParams = new URLSearchParams(url.search);
 	try {
 		// TODO: use env var
 		const db = mongoClient.db("kijiji-map");
 
-		const url = new URL(req.url);
-		const searchParams = new URLSearchParams(url.search);
 		const filtersParam = searchParams.get("filters");
 
-		if (!filtersParam) return NextResponse.error();
+		if (!filtersParam)
+			return new Response("Bad Request", {
+				status: 400,
+			});
 
 		const decodedParams = decodeURIComponent(filtersParam);
 		const parsedParams: TFilters = JSON.parse(decodedParams);
