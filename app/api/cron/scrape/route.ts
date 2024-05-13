efbb8ac9e5ada5a55b891f964b4c972105a69c1c
kornelsen <matt@kornelsen.dev>
2024-05-13 09:46:43 -0400

    check for scrape api auth header outside of try/catch

--- app/api/cron/scrape/route.ts
+++ app/api/cron/scrape/route.ts
@@ -6,14 +6,13 @@ import mongoClient from "../../../_lib/mongodb";
 import { mapToListing } from "../../listings/utils";
 
 export async function GET(request: NextRequest) {
+	const authHeader = request.headers.get("authorization");
+	if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+		return new Response("Unauthorized", {
+			status: 401,
+		});
+	}
 	try {
-		const authHeader = request.headers.get("authorization");
-		if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-			return new Response("Unauthorized", {
-				status: 401,
-			});
-		}
-
 		console.log("Starting scraping process.");
 		const resp = await search(
 			{
