0f3b13caa153bb67a64a8a58c2d3b4dd45d53f3f
Matt Kornelsen <matt@kornelsen.dev>
2024-09-06 09:27:31 -0400

    create constants file for filters

---
+++ app/_components/listings/filters/filters.constants.ts
@@ -0,0 +1,28 @@
+export const bedOptions = [
+  { label: "0", value: 0 },
+  { label: "1", value: 1 },
+  { label: "1.5", value: 1.5 },
+  { label: "2", value: 2 },
+  { label: "2.5", value: 2.5 },
+  { label: "3", value: 3 },
+];
+
+export const bathOptions = [
+  { label: "1", value: 1 },
+  { label: "2", value: 2 },
+  { label: "3", value: 3 },
+];
+
+export const miscOptions = [
+  { label: "A/C", value: "airconditioning" },
+  { label: "Parking", value: "numberparkingspots" },
+  { label: "Dishwasher", value: "dishwasher" },
+  { label: "Laundry", value: "laundryinunit" },
+  { label: "Furnished", value: "furnished" },
+  { label: "Balcony", value: "balcony" },
+  { label: "Locker", value: "storagelocker" },
+  { label: "Gym", value: "gym" },
+  { label: "Pool", value: "pool" },
+  { label: "Elevator", value: "elevator" },
+  { label: "Concierge", value: "concierge" },
+];
--- app/_components/listings/filters/filters.tsx
+++ app/_components/listings/filters/filters.tsx
@@ -12,6 +12,7 @@ import {
   AccordionContent,
 } from "../../ui/accordion";
 import { Slider } from "../../ui/slider";
+import { bathOptions, bedOptions, miscOptions } from "./filters.constants";
 
 export const Filters = () => {
   const { filters: filtersStoreState, updateFilters: updateFiltersStore } =
@@ -121,37 +122,6 @@ export const Filters = () => {
     </Accordion>
   );
 };
-
-// TODO: move these to another file
-
-const bedOptions = [
-  { label: "0", value: 0 },
-  { label: "1", value: 1 },
-  { label: "1.5", value: 1.5 },
-  { label: "2", value: 2 },
-  { label: "2.5", value: 2.5 },
-  { label: "3", value: 3 },
-];
-
-const bathOptions = [
-  { label: "1", value: 1 },
-  { label: "2", value: 2 },
-  { label: "3", value: 3 },
-];
-
-const miscOptions = [
-  { label: "A/C", value: "airconditioning" },
-  { label: "Parking", value: "numberparkingspots" },
-  { label: "Dishwasher", value: "dishwasher" },
-  { label: "Laundry", value: "laundryinunit" },
-  { label: "Furnished", value: "furnished" },
-  { label: "Balcony", value: "balcony" },
-  { label: "Locker", value: "storagelocker" },
-  { label: "Gym", value: "gym" },
-  { label: "Pool", value: "pool" },
-  { label: "Elevator", value: "elevator" },
-  { label: "Concierge", value: "concierge" },
-];
 
 const getActiveFiltersCount = (filters: TFilters) => {
   const excludedFilters = ["bounds", "misc"];
