225fe801530be4a05afd38fe1ffee7981eb07e99
Matt Kornelsen <matt@kornelsen.dev>
2024-09-07 16:11:58 -0400

    debug prod

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import {
   HydrationBoundary,
   QueryClient,
   dehydrate,
+  isServer,
 } from "@tanstack/react-query";
 import { Listings } from "./_components/listings";
 import { getListings, initialFilters } from "./api/listings";
 
 export default async function Home() {
   const queryClient = new QueryClient();
+
+  console.log("this is the server", isServer);
 
   await queryClient.prefetchQuery({
     queryKey: ["listings", encodeURIComponent(JSON.stringify(initialFilters))],
