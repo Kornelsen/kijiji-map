6afaca99c993308b04d890d4d428960a08e1a2a3
Kornelsen <matt@kornelsen.dev>
2024-10-02 07:59:43 -0400

    turn off vercel image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media.kijiji.ca",
-        port: "",
-        pathname: "/api/v1/**/images/**/**",
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "media.kijiji.ca",
+    //     port: "",
+    //     pathname: "/api/v1/**/images/**/**",
+    //   },
+    // ],
+    unoptimized: true,
   },
 };
 
